887d2d1e9b02157d3960620560f615d690dce7ca
Jarosław Pelczar <jarek@jpelczar.com>
2026-06-10 07:27:42 +0200

    Update model code
    Signed-off-by: Jarosław Pelczar <jarek@jpelczar.com>

--- include/ftxmodel/shortcut_action_model.hpp
+++ include/ftxmodel/shortcut_action_model.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <ranges>
+
 #include "abstract_list_model.hpp"
 #include "ftxui/component/event.hpp"
 
@@ -26,6 +28,30 @@ class ShortcutActionModel : public AbstractListModel {
     setItems(std::move(items));
   }
 
+  ShortcutItem* itemAt(const int index) const noexcept {
+    if (index < 0 || index >= static_cast<int>(m_items.size())) {
+      return nullptr;
+    } else {
+      return m_items[static_cast<size_t>(index)].get();
+    }
+  }
+
+  void addItem(const ShortcutItem& item) {
+    const int count = static_cast<int>(m_items.size());
+    beginInsertRows(ModelIndex(), count, count);
+    m_items.emplace_back(std::make_unique<ShortcutItem>(std::move(item)));
+    endInsertRows();
+  }
+
+  void removeItem(int row) {
+    if (row < 0 || row >= static_cast<int>(m_items.size())) {
+      return;
+    }
+    beginRemoveRows(ModelIndex(), row, row);
+    m_items.erase(m_items.begin() + static_cast<size_t>(row));
+    endRemoveRows();
+  }
+
   void setItems(std::vector<ShortcutItem> items) {
     beginResetModel();
     m_items.clear();
@@ -36,21 +62,23 @@ class ShortcutActionModel : public AbstractListModel {
     endResetModel();
   }
 
-  int rowCount(const ModelIndex& parent) const override {
+  int rowCount(const ModelIndex& parent = ModelIndex()) const override {
     return parent.isValid() ? 0 : static_cast<int>(m_items.size());
   }
 
-  bool hasChildren(const ModelIndex& parent) const override {
+  bool hasChildren(const ModelIndex& parent = ModelIndex()) const override {
     return parent.isValid() ? false : !m_items.empty();
   }
 
-  std::any data(const ModelIndex& index, ItemRole role) const override {
+  std::any data(const ModelIndex& index, const ItemRole role) const override {
     if (!index.isValid() || index.row() >= rowCount(index)) {
       return {};
     }
 
     const auto* item = m_items[static_cast<size_t>(index.row())].get();
     switch (role) {
+      case ItemRole::UniqueIdentifierRole:
+        return index.row();
       case ItemRole::DisplayRole:
         return item->name;
       case KeyRole:
@@ -71,6 +99,21 @@ class ShortcutActionModel : public AbstractListModel {
       f |= ItemFlag::ItemIsEditable;
     }
     return f;
+  }
+
+  ModelIndex findIndexById(const UniqueNodeId& targetId,
+                           const ModelIndex& parent) const override {
+    if (parent.isValid()) {
+      return {};
+    }
+    if (!std::holds_alternative<int64_t>(targetId)) {
+      return {};
+    }
+    const int row = static_cast<int>(std::get<int64_t>(targetId));
+    if (row < 0 || row >= static_cast<int>(m_items.size())) {
+      return {};
+    }
+    return index(row, 0, parent);
   }
 
  protected:
